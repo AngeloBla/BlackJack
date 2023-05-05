8611be57623698b7f6084c49a1f9dfbdad54591d
PasiGu <122011698+PasiGu@users.noreply.github.com>
2023-05-05 14:36:59 +0200

    Update test.js
    neu

--- test.js
+++ test.js
@@ -3,3 +3,18 @@ function hallo(name){
 }
 console.log(hallo("Ango"));
 
+<<<<<<< Updated upstream
+=======
+function pasta(Pastarezept) {
+    console.log("Pastarezept")
+    console.log("Die Soße bereitest du idealerweise vor der Pasta zu, sie sollte jetzt griffbereit sein.")
+    console.log("Das aufgefangene Nudelwasser kann der Soße durch die enthaltene Stärke, falls nötig, noch etwas Bindung geben.")
+    console.log("In Italien mischt man die Nudeln und die Soße sofort in einer großen, vorgewärmten Schüssel.")
+    console.log("So haftet die Soße am besten an den Nudeln.")
+}
+pasta();
+
+
+
+mal sehen ob das stimmt was Eddi erzaehlt
+>>>>>>> Stashed changes
