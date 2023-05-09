9c0252b4d23f381ddf0208fe6b131ec13009ca2e
AngeloBla <angeloblajic@gmail.com>
2023-05-09 12:08:22 +0200

    Update BlackJack.js
    Weitere Funktionen hinzugefügt:
    -Bank Karten prüfen
    -neu beginnen
    -Bank zieht eine Karte
    Funktionen verbessert:
    -Püfe Spielstand des Spielers
    var "spieler" und "bank" zu let geändert um sie in anderen funktionen anzusprechen.
    Allgemeinen Code verbessert und Logikfehler behoben.

--- BlackJack.js
+++ BlackJack.js
@@ -1,29 +1,30 @@
-let spieler = [];
-let bank = [];
 const Bube = 10;
 const Dame = 10;
 const König = 10;
 const Ass = 11;
 const deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, Bube, Dame, König, Ass];
 
+let spieler = [];
+let bank = [];
+
 function karten_austeilen() {
   for (let i = 0; i < 2; i++) {
     const randomIndex = Math.floor(Math.random() * deck.length);
     spieler.push(deck[randomIndex]);
-    deck.splice(randomIndex, 1); 
+    deck.splice(randomIndex, 1);
   }
-  
+
   for (let i = 0; i < 2; i++) {
     const randomIndex = Math.floor(Math.random() * deck.length);
     bank.push(deck[randomIndex]);
-    deck.splice(randomIndex, 1); 
+    deck.splice(randomIndex, 1);
   }
 }
 
 function karte_ziehen() {
   const randomIndex = Math.floor(Math.random() * deck.length);
   spieler.push(deck[randomIndex]);
-  deck.splice(randomIndex, 1); 
+  deck.splice(randomIndex, 1);
 }
 
 function pruefe_spielstand() {
@@ -33,15 +34,18 @@ function pruefe_spielstand() {
   }
   if (summeSpieler > 21) {
     console.log("Sie haben verloren. Ihre Karten haben einen Wert von " + summeSpieler);
-    neu_beginnen();
-  }
-  else if (summeSpieler === 21) {
+  } else if (summeSpieler === 21) {
     console.log("Sie haben GEWONNEN! Ihre Karten haben einen Wert von " + summeSpieler);
-  }
-  else if (summeSpieler < 21) {
+  } else if (summeSpieler < 21) {
     console.log("Unter 21. Möchten Sie eine weitere Karte ziehen?");
     karte_ziehen();
   }
+}
+
+function karte_ziehen_Bank() {
+  const randomIndex = Math.floor(Math.random() * deck.length);
+  bank.push(deck[randomIndex]);
+  deck.splice(randomIndex, 1);
 }
 
 function pruefe_bank() {
@@ -51,26 +55,20 @@ function pruefe_bank() {
   }
   if (summeBank < 18) {
     karte_ziehen_Bank();
+    pruefe_spielstand();
   }
 }
 
-function karte_ziehen_Bank() {
-  const randomIndex = Math.floor(Math.random() * deck.length);
-  bank.push(deck[randomIndex]);
-  deck.splice(randomIndex, 1); 
-}
-
-function neu_beginnen(){
+function neu_beginnen() {
   spieler = [];
   bank = [];
+  karten_austeilen();
 }
 
 karten_austeilen();
-console.log("Der Spieler hat: "+spieler); 
-console.log("Die Bank hat: "+bank); 
+console.log("Der Spieler hat: " + spieler);
+console.log("Die Bank hat: " + bank);
 pruefe_spielstand();
 pruefe_bank();
-console.log("Der Spieler hat: "+spieler); 
-console.log("Die Bank hat: "+bank); 
-pruefe_spielstand();
-pruefe_bank();
+console.log("Der Spieler hat: " + spieler);
+console.log("Die Bank hat: " + bank);
