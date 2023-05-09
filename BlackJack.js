84f7a18dc3b5377bcdd39f28787bfa69af8dce17
AngeloBla <angeloblajic@gmail.com>
2023-05-09 10:23:35 +0200

    Dame mit zwei e

--- BlackJack.js
+++ BlackJack.js
@@ -1,7 +1,7 @@
 var spieler = [];
 var bank = [];
 const Bube = 10;
-const Dame = 10;
+const Damee = 10;
 const König = 10;
 const Ass = 11;
 const deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, Bube, Dame, König, Ass];
